83cf401c2543409dc3e09380d0b104014433cbf2
Kamil <kamilf20@gmail.com>
2019-05-28 21:39:20 +0200

    Dodaj pliki projektów.

---
+++ GameOfLife/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace GameOfLife
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(550, 550);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(601, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Rozmiar komorki siatki";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(604, 208);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(122, 24);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(601, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Stany początkowe";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(604, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Rozpocznij";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(604, 87);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.NumericUpDown1_ValueChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(604, 319);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(84, 21);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Wykonuj";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(604, 453);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Iteracja";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(568, 417);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(209, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Każda iteracja po kolejnym kliku";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(601, 287);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Ciągłość iteracji";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 582);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Game of Life";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ GameOfLife/Form1.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    public partial class Form1 : Form
+    {
+
+        private Bitmap bitmap;
+        private Graphics graphics;
+        private int vertical_cells_num;
+        private int horizontal_cells_num;
+        private int cell_size_value;
+        private bool[,] cells_status;
+        private string pattern;
+        public Form1()
+        {
+            InitializeComponent();
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(bitmap);
+
+            Patterns();
+            Change_grid_size();
+            Grid_size();
+        }
+
+        private void Grid_size()
+        {
+            horizontal_cells_num = pictureBox1.Height / cell_size_value;
+            vertical_cells_num = pictureBox1.Width / cell_size_value;
+
+            cells_status = new bool[horizontal_cells_num, vertical_cells_num];
+
+            Match_Pattern();
+
+            PrintGrid();
+        }
+
+        private void Match_Pattern()
+        {
+            switch (pattern)
+            {
+                case "Empty":
+                    {
+                        break;
+                    }
+               
+                case "Niezmienne":
+                    {
+                        cells_status[horizontal_cells_num / 2 - 1, vertical_cells_num / 2] = true;
+                        cells_status[horizontal_cells_num / 2 - 1, vertical_cells_num / 2 + 1] = true;
+                        cells_status[horizontal_cells_num / 2 , vertical_cells_num / 2 - 1] = true;
+                        cells_status[horizontal_cells_num / 2 , vertical_cells_num / 2 + 2] = true;
+                        cells_status[horizontal_cells_num / 2 + 1, vertical_cells_num / 2] = true;
+                        cells_status[horizontal_cells_num / 2 + 1, vertical_cells_num / 2 + 1] = true;
+
+                        break;
+                    }
+
+                case "Oscylator":
+                    {
+                        cells_status[horizontal_cells_num / 2 - 1, vertical_cells_num / 2] = true;
+                        cells_status[horizontal_cells_num / 2, vertical_cells_num / 2] = true;
+                        cells_status[horizontal_cells_num / 2 + 1, vertical_cells_num / 2] = true;
+                        break;
+                    }
+                
+                case "Glider":
+                    {
+                        cells_status[horizontal_cells_num / 2, vertical_cells_num / 2] = true;
+                        cells_status[horizontal_cells_num / 2 - 1, vertical_cells_num / 2 - 1] = true;
+                        cells_status[horizontal_cells_num / 2, vertical_cells_num / 2 + 1] = true;
+                        cells_status[horizontal_cells_num / 2 + 1, vertical_cells_num / 2] = true;
+                        cells_status[horizontal_cells_num / 2 + 1, vertical_cells_num / 2 - 1] = true;
+
+                        break;
+                    }
+ 
+                case "Random":
+                    {
+                        Random rand = new Random();
+                        for (int i = 0; i < horizontal_cells_num; i++)
+                        {
+                            for (int j = 0; j < vertical_cells_num; j++)
+                            {
+                                cells_status[i, j] = rand.NextDouble() >= 0.6;
+                            }
+                        }
+
+                        break;
+                    }
+            }
+        }
+
+        private void Change_grid_size()
+        {
+            numericUpDown1.Minimum = 2;
+            numericUpDown1.Maximum = 100;
+            numericUpDown1.Value = 50;
+
+        }
+
+        private void Patterns()
+        {
+            comboBox1.Items.Add("Empty");            
+            comboBox1.Items.Add("Niezmienne");
+            comboBox1.Items.Add("Oscylator");           
+            comboBox1.Items.Add("Glider");
+            comboBox1.Items.Add("Random"); 
+        }
+
+        private void Start_Game_iterations()
+        {
+
+            while (checkBox1.Checked)
+            {
+
+                Rules();
+                PrintGrid();
+                Thread.Sleep(1000);
+            }
+
+        }
+
+        private void Start_Game_step_by_step()
+        {
+            Rules();
+            PrintGrid();
+            Thread.Sleep(1000);
+
+        }
+
+        private void Rules()
+        {
+            bool[,] sasiednia_status = new bool[horizontal_cells_num, vertical_cells_num];
+
+            for (int i = 0; i < horizontal_cells_num; i++)
+            {
+                for (int j = 0; j < vertical_cells_num; j++)
+                {
+                    int sasiedzi = Count_Neighbours(i, j);
+                    bool zywa = cells_status[i, j];
+
+                    if (zywa)
+                    {
+                        //umiera
+                        if (sasiedzi < 2)
+                        {
+                            sasiednia_status[i, j] = false;
+                        }
+                        else if (sasiedzi > 3)
+                        {
+                            sasiednia_status[i, j] = false;
+                        }
+                        else
+                        {
+                            sasiednia_status[i, j] = true;
+                        }
+                    }
+                    else
+                    {
+                       //staje sie zywa
+                        if (sasiedzi == 3)
+                        {
+                            sasiednia_status[i, j] = true;
+                        }
+                        else
+                        {
+                            sasiednia_status[i, j] = false;
+                        }
+                    }
+                }
+            }
+
+            cells_status = sasiednia_status;
+        }
+
+        private int Count_Neighbours(int x, int y)
+        {
+            int count = 0;
+
+            for (int i = x - 1; i <= x + 1; i++)
+            {
+                for (int j = y - 1; j <= y + 1; j++)
+                {
+                    if (i == x && j == y)
+                    {
+                        continue;
+                    }
+
+                    int index_x = i;
+                    int index_y = j;
+
+                    if (i == -1)
+                    {
+                        index_x = horizontal_cells_num - 1;
+                    }
+                    else if (i == horizontal_cells_num)
+                    {
+                        index_x = 0;
+                    }
+
+                    if (j == -1)
+                    {
+                        index_y = vertical_cells_num - 1;
+                    }
+                    else if (j == vertical_cells_num)
+                    {
+                        index_y = 0;
+                    }
+
+                    if (cells_status[index_x, index_y])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        private void PrintGrid()
+        {
+            lock (graphics)
+            {
+                graphics.Clear(Color.White);
+                Pen black_pen = new Pen(Color.Black);
+                Brush black_brush = new SolidBrush(Color.DarkGray);
+
+                for (int i = 0; i < horizontal_cells_num + 1; i++)
+                {
+                    graphics.DrawLine(black_pen, new Point(0, i * cell_size_value), new Point(vertical_cells_num * cell_size_value, i * cell_size_value));
+                }
+
+                for (int i = 0; i < vertical_cells_num + 1; i++)
+                {
+                    graphics.DrawLine(black_pen, new Point(i * cell_size_value, 0), new Point(i * cell_size_value, horizontal_cells_num * cell_size_value));
+                }
+
+                for (int i = 0; i < horizontal_cells_num; i++)
+                {
+                    for (int j = 0; j < vertical_cells_num; j++)
+                    {
+                        if (cells_status[i, j])
+                        {
+                            Rectangle rectangle = new Rectangle(
+                                j * cell_size_value,
+                                i * cell_size_value,
+                                cell_size_value,
+                                cell_size_value
+                            );
+
+                            graphics.FillRectangle(black_brush, rectangle);
+                        }
+                    }
+                }
+
+                pictureBox1.Image = bitmap;
+            }
+        }
+
+
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            Point coordinates = me.Location;
+
+            int i = coordinates.Y / cell_size_value;
+            int j = coordinates.X / cell_size_value;
+
+            bool status = cells_status[i, j];
+
+            if (status)
+            {
+                cells_status[i, j] = false;
+            }
+            else
+            {
+                cells_status[i, j] = true;
+            }
+
+            PrintGrid();
+        }
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+            pattern = comboBox1.SelectedItem.ToString();
+            Grid_size();
+        }
+
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                Thread thr = new Thread(Start_Game_iterations);
+                thr.IsBackground = true;
+                thr.Start();
+            }
+
+        }
+
+        private void NumericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+            cell_size_value = decimal.ToInt32(numericUpDown1.Value);
+            Grid_size();
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+            Thread thr = new Thread(Start_Game_step_by_step);
+            thr.IsBackground = true;
+            thr.Start();
+
+        }
+    }
+}
